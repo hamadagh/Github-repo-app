56bb28271e90bb002e009d2e1d6acabb2b48229e
Ahmad <hamadagh777@gmail.com>
2020-03-14 18:17:23 +0100

    adding expansion panel to OpenedIssues component

--- src/components/repoDetails/details/OpenedIssues.js
+++ src/components/repoDetails/details/OpenedIssues.js
@@ -2,31 +2,107 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import MuiExpansionPanel from '@material-ui/core/ExpansionPanel';
+import MuiExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import MuiExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 
+import Button from '@material-ui/core/Button';
+
+const ExpansionPanel = withStyles({
+    root: {
+        width: '100%',
+        border: '1px solid rgba(0, 0, 0, .125)',
+        boxShadow: 'none',
+        '&:not(:last-child)': {
+            borderBottom: 0,
+        },
+        '&:before': {
+            display: 'none',
+        },
+        '&$expanded': {
+            marginTop: '200px',
+        },
+    },
+    expanded: {},
+})(MuiExpansionPanel);
+
+const ExpansionPanelSummary = withStyles({
+    root: {
+        backgroundColor: 'rgba(0, 0, 0, .03)',
+        borderBottom: '1px solid rgba(0, 0, 0, .125)',
+        marginTop: -1,
+        minHeight: 156,
+        '&$expanded': {
+            minHeight: 56,
+
+        },
+    },
+    content: {
+        '&$expanded': {
+
+        },
+    },
+    expanded: {},
+})(MuiExpansionPanelSummary);
+
+const ExpansionPanelDetails = withStyles(theme => ({
+    root: {
+        padding: theme.spacing(2),
+        marginBottom: '10px',
+        height: 'auto',
+    },
+}))(MuiExpansionPanelDetails);
 const useStyles = makeStyles(theme => ({
-    paper: {
+    root: {
         display: 'flex',
         flexWrap: 'wrap',
-        marginBottom: '10px',
+        width: '100%',
         '& > *': {
             margin: theme.spacing(1),
             width: '100%',
-            height: theme.spacing(16),
+            height: 'auto',
         },
     },
+    paper: {
+        width: '100%',
+        height: '100%',
+        textAlign: 'center',
+
+    }
 }));
 
 function OpenedIssues(data) {
     const classes = useStyles();
+    const [expanded, setExpanded] = React.useState('');
+    console.log(data)
+    const handleChange = panel => (event, newExpanded) => {
+        setExpanded(newExpanded ? panel : false);
+    };
+
 
     return (
+
         <div className="opened-issues">
             {
                 data.data.nodes.map((data, index) =>
-                    <Paper children={<Typography>{data.title}</Typography>} className={classes.paper} elevation={3} key={index} />
+                    <Button color="primary" key={index} className={classes.root}>
+                        <ExpansionPanel square expanded={expanded === `panel${index}`} onChange={handleChange(`panel${index}`)}>
+                            <ExpansionPanelSummary aria-controls="panel1d-content" id="panel2d-header">
+                                <Typography>{data.title}</Typography>
+                            </ExpansionPanelSummary>
+                            <ExpansionPanelDetails>
+                                <Typography>
+                                    {data.body}
+                                </Typography>
+                            </ExpansionPanelDetails>
+                        </ExpansionPanel>
+                    </Button>
                 )
             }
         </div>
+
+
     );
 }
 
